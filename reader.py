aa08aa92d556fbb679e0205ba8e45f3ad0038c1e
Brandon Lahmann <brandon@lahmann.io>
2025-07-20 20:23:22 -0700

    Reworked structure to not store file

--- reader.py
+++ reader.py
@@ -5,6 +5,33 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
+
+def _read_next_value(f, code, prefix='<'):
+    size = struct.calcsize(code)
+    data = f.read(size)
+    return struct.unpack(f'{prefix}{code}', data)[0]
+
+
+def _read_next_int(f):
+    return _read_next_value(f, 'i')
+
+
+def _read_next_float(f):
+    return _read_next_value(f, 'f')
+
+
+def _read_next_short(f):
+    return _read_next_value(f, 'h')
+
+
+def _read_next_byte(f):
+    return _read_next_value(f, 'b')
+
+
+def _skip_forward(f, n):
+    f.read(n)
+
+
 class ScanData:
     def __init__(self, path, frame_buffer_size=np.inf, track_buffer_size=np.inf, d_bounds=(0, np.inf), e_bounds=(0, np.inf), c_bounds=(0, np.inf), a_bounds=(0, np.inf), x_bounds=(-np.inf, np.inf), y_bounds=(-np.inf, np.inf)):
         self.trailer = None
@@ -29,48 +56,47 @@ class ScanData:
 
         try:
             with open(path, 'rb') as f:
-                self._f = f
-                self._parse(frame_buffer_size, track_buffer_size, d_bounds, e_bounds, c_bounds, a_bounds, x_bounds, y_bounds)
+                self._parse(f, frame_buffer_size, track_buffer_size, d_bounds, e_bounds, c_bounds, a_bounds, x_bounds, y_bounds)
         except Exception as e:
             traceback.print_exception(e)
 
-    def _parse(self, frame_buffer_size, track_buffer_size, d_bounds, e_bounds, c_bounds, a_bounds, x_bounds, y_bounds):
-        self._parse_header()
-        self._parse_data(frame_buffer_size, track_buffer_size, d_bounds, e_bounds, c_bounds, a_bounds, x_bounds, y_bounds)
-        self._parse_trailer()
+    def _parse(self, f, frame_buffer_size, track_buffer_size, d_bounds, e_bounds, c_bounds, a_bounds, x_bounds, y_bounds):
+        self._parse_header(f)
+        self._parse_data(f, frame_buffer_size, track_buffer_size, d_bounds, e_bounds, c_bounds, a_bounds, x_bounds, y_bounds)
+        self._parse_trailer(f)
 
-    def _parse_header(self):
+    def _parse_header(self, f):
         self.header = {
-            'version_number': self.get_next_int(),
-            'num_x_frames': self.get_next_int(),
-            'num_y_frames': self.get_next_int(),
-            'num_bins': self.get_next_int(),
-            'pixel_size': 1e-4 * self.get_next_float(),
-            'pixels_per_bin': self.get_next_float(),
-            'border_limit': self.get_next_int(),
-            'contrast_limit': self.get_next_int(),
-            'eccentricity_limit': self.get_next_int(),
-            'M': self.get_next_int(),
-            'frame_width': self.get_next_int(),
-            'frame_height': self.get_next_int(),
+            'version_number': _read_next_int(f),
+            'num_x_frames': _read_next_int(f),
+            'num_y_frames': _read_next_int(f),
+            'num_bins': _read_next_int(f),
+            'pixel_size': 1e-4 * _read_next_float(f),
+            'pixels_per_bin': _read_next_float(f),
+            'border_limit': _read_next_int(f),
+            'contrast_limit': _read_next_int(f),
+            'eccentricity_limit': _read_next_int(f),
+            'M': _read_next_int(f),
+            'frame_width': _read_next_int(f),
+            'frame_height': _read_next_int(f),
         }
         self.header['frame_width'] *= self.header['pixel_size']
         self.header['frame_height'] *= self.header['pixel_size']
 
-    def _parse_data(self, frame_buffer_size, track_buffer_size, d_bounds, e_bounds, c_bounds, a_bounds, x_bounds, y_bounds):
+    def _parse_data(self, f, frame_buffer_size, track_buffer_size, d_bounds, e_bounds, c_bounds, a_bounds, x_bounds, y_bounds):
         frame_buffer = []
         track_buffer = []
         num_frames = self.header['num_x_frames'] * self.header['num_y_frames']
         try:
             for _ in tqdm(range(num_frames)):
-                number = self.get_next_int()
-                x_position = 1e-5 * self.get_next_int()
-                y_position = 1e-5 * self.get_next_int()
-                num_tracks = self.get_next_int()
-                self.skip(12)
-                focus = 1e-2 * self.get_next_int()
-                x_position_index = self.get_next_int()
-                y_position_index = self.get_next_int()
+                number = _read_next_int(f)
+                x_position = 1e-5 * _read_next_int(f)
+                y_position = 1e-5 * _read_next_int(f)
+                num_tracks = _read_next_int(f)
+                _skip_forward(f, 12)
+                focus = 1e-2 * _read_next_int(f)
+                x_position_index = _read_next_int(f)
+                y_position_index = _read_next_int(f)
 
                 frame_buffer.append({
                     'number': number,
@@ -86,12 +112,12 @@ class ScanData:
                     self.frames = pd.concat([self.frames, pd.DataFrame(frame_buffer)], ignore_index=True)
                     frame_buffer.clear()
 
-                d_array = [self.get_next_short() for _ in range(num_tracks)]
-                e_array = [self.get_next_byte() for _ in range(num_tracks)]
-                c_array = [self.get_next_byte() for _ in range(num_tracks)]
-                a_array = [self.get_next_byte() for _ in range(num_tracks)]
-                x_array = [self.get_next_short() for _ in range(num_tracks)]
-                y_array = [self.get_next_short() for _ in range(num_tracks)]
+                d_array = [_read_next_short(f) for _ in range(num_tracks)]
+                e_array = [_read_next_byte(f) for _ in range(num_tracks)]
+                c_array = [_read_next_byte(f) for _ in range(num_tracks)]
+                a_array = [_read_next_byte(f) for _ in range(num_tracks)]
+                x_array = [_read_next_short(f) for _ in range(num_tracks)]
+                y_array = [_read_next_short(f) for _ in range(num_tracks)]
 
                 for (d, e, c, a, x, y) in zip(d_array, e_array, c_array, a_array, x_array, y_array):
                     d_um = 100 * d * self.header['pixel_size']
@@ -122,26 +148,6 @@ class ScanData:
             if track_buffer:
                 self.tracks = pd.concat([self.tracks, pd.DataFrame(track_buffer)], ignore_index=True)
 
-    def _parse_trailer(self):
-        self.skip(4)
-        self.trailer = self._f.read().decode('latin-1')
-
-    def get_next_value(self, code, prefix='<'):
-        size = struct.calcsize(code)
-        data = self._f.read(size)
-        return struct.unpack(f'{prefix}{code}', data)[0]
-
-    def get_next_int(self):
-        return self.get_next_value('i')
-
-    def get_next_float(self):
-        return self.get_next_value('f')
-
-    def get_next_short(self):
-        return self.get_next_value('h')
-
-    def get_next_byte(self):
-        return self.get_next_value('b')
-
-    def skip(self, n):
-        self._f.read(n)
+    def _parse_trailer(self, f):
+        _skip_forward(f, 4)
+        self.trailer = f.read().decode('latin-1')
--- test.py
+++ test.py
@@ -2,10 +2,10 @@ from reader import ScanData
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
-data = ScanData('O109043_CPS2_d9w_dw7813_4.5hr_40x_s0.cpsa', c_bounds=(0, 50), x_bounds=(0.0, 1.2), y_bounds=(-1.5, 1.0))
+data = ScanData('O109043_CPS2_d9w_dw7813_4.5hr_40x_s0.cpsa', d_bounds=(5.0, 20.0), c_bounds=(0, 50), x_bounds=(0.0, 1.2), y_bounds=(-1.5, 1.0))
 tracks = data.tracks
 
-plt.hist2d(tracks['d'], tracks['c'], bins=[500, 50], norm=LogNorm())
+plt.hist2d(tracks['d'], tracks['c'], bins=[500, 48], norm=LogNorm())
 plt.xlim([0, 20])
 plt.show()
 
